a5ee037c380b0d38fed03c4a79a1199f9198bf34
Md. Samiul Islam <95273981+MDSAMIULSAMI@users.noreply.github.com>
2024-08-11 17:09:47 +0600

    urls.py

--- backend/property_listing/listings/urls.py
+++ backend/property_listing/listings/urls.py
@@ -1,4 +1,3 @@
-# listings/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PropertyViewSet, PropertyImageViewSet, BidViewSet, TestimonialViewSet, TeamMemberViewSet
